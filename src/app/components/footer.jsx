806e72e7f64ca106368ccf8705b1d40c160fee7f
SFNTK <153030492+SFNTK@users.noreply.github.com>
2025-01-21 21:09:37 +0100

    Update footer.jsx

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
             <div id='contactinfo' className=' flex justify-between flex-col md:flex-row gap-x-5 gap-y-3 flex-wrap '>
 
                 <div className='flex font-prmr font-bold   gap-x-1 items-center  text-dark'><img id='contactimg' src="./image1copy2.jpeg" alt="" /> <span>SOUFIANE TAKI</span></div>
-                <div className='flex gap-x-1 font-prmr   items-center text-dark'><Mail />contact@soufianetaki.info</div>
+               
                 <div className='flex gap-x-1 font-prmr   items-center  text-dark'><Mail />sfentaki@gmail.com</div>
                 <div className='flex gap-x-1 font-prmr  items-center  text-dark'><Mail />takisfen@gmail.com</div>
 
